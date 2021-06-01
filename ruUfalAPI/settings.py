f61760a74425f83fdd6f624d05c48afbb3e9373c
SamillyNunes <samillynunes19@gmail.com>
2021-06-01 11:46:07 -0300

    Settings

--- ruUfalAPI/settings.py
+++ ruUfalAPI/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
