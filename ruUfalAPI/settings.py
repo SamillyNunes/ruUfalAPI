e1bb7043b8f1eddf68fbc6eebe60ac5873008bdf
SamillyNunes <samillynunes19@gmail.com>
2021-06-01 11:40:06 -0300

    Settings

--- ruUfalAPI/settings.py
+++ ruUfalAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pmok7m$$+a8@rn^%*8^-hv!9tgx_%57!ats=(u_6q$d+h)emm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
