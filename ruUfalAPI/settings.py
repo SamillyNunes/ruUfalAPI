ed66962d2f331cdb96c535aab476bf879a19d3ae
SamillyNunes <samillynunes19@gmail.com>
2021-06-01 12:25:15 -0300

    Turning off debug

--- ruUfalAPI/settings.py
+++ ruUfalAPI/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'pmok7m$$+a8@rn^%*8^-hv!9tgx_%57!ats=(u_6q$d+h)emm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
